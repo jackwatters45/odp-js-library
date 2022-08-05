6bcf2acb1edaf011077f89cbd27bbbe4476c87ae
Jack Watters <johnwatt@usc.edu>
2022-08-05 01:23:09 -0700

    set up html and css content. About finished.

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+
+console.log('cock');
+console.log(9 + 10);
+let twentyOne = 19;
+console.log(twentyOne / 10)
+
+// useful to remember
+// Arrow functions 
+// textBox.addEventListener('keydown', (event) => {
+// console.log(`You pressed "${event.key}".`);
+// });
+
+// create elements
+// const div = document.createElement('div');
+
+// select dom elements
+// const container = document.querySelector('#container');
+
+// insert element into page
+// parentNode.appendChild(childNode) appends childNode as the last child of parentNode
+// parentNode.insertBefore(newNode, referenceNode) inserts newNode into parentNode before referenceNode
+
+// inline style
+// div.style.color = 'blue';                                      
+
+// button
+const btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+  // alert("Hello World");
+// });
+
+// consider named functions
+btn.addEventListener('click', function (e) {
+  console.log(e);
+});
+
+btn.addEventListener('click', function (e) {
+  console.log(e.target);
+});
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
+
+// beeeeep
 let myLibrary = [
 
 ];
