cc43a1377594fed5a57bf2e71ff6a6635408512a
Jack Watters <johnwatt@usc.edu>
2022-08-31 13:18:21 -0700

    finished library and book classes. added real backend functionality (this.books).

---
+++ README.md
@@ -0,0 +1,3 @@
+add functionality for isRead
+
+probably make the forms themselves an object at some point
--- script.js
+++ script.js
@@ -1,109 +1,6 @@
-// I don't have brain power to add the toggle part now but do when you revisit!!!
-
-// Add a button on each book’s display to change its read status.
-// To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
-
-// Books stored in this array
-// let myLibrary = [];
-
-// CONSTRUCTOR
-// function Book(title, author = "Unknown", numPages = "Unknown", read = false) {
-//   this.author = author;
-//   this.title = title;
-//   this.numPages = numPages;
-//   this.isRead = read;
-// }
-
 // Page Elements
-let form = document.forms["book-input"];
-let mainContent = document.querySelector(".main-content");
 let newBooks = document.querySelector(".new-book-btn");
-let removeButtons = document.querySelectorAll(".remove-btn");
-
-class Book {
-  constructor(title, author = "Unknown", numPages = "Unknown", read = false) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.read = read;
-  }
-
-  get title() {
-    return this.title;
-  }
-}
-
-// list of books
-class Library {
-  constructor() {
-    this.books = [];
-  }
-
-  get book() {
-    return this.books;
-  }
-
-  set book(newBook) {
-    this.books.push(newBook);
-  }
-}
-
-myLibrary = new Library();
-
-// take user’s input and store the new book objects into an array
-function addBook() {
-  form.addEventListener("submit", (e) => {
-    console.log('click on form ain t workin')
-    e.preventDefault();
-
-    // Get form values
-    let title = form.querySelector(".title-data").value;
-    let author = form.querySelector(".author-data").value;
-    let numPages = form.querySelector(".num-pages-data").value;
-    let isRead = form.querySelector(".read-data").checked;
-
-    // Create book object
-    let book = new Book(title, author, numPages, isRead);
-
-    // Add book to array
-    myLibrary.addToLibrary(book);
-
-    // Remove form
-    form.parentNode.removeChild(form);
-
-    console.log('click but display books aint working')
-    // Display Books
-    displayBooks();
-  });
-};
-
-// function that loops through mylibrary and displays each book on the page.
-function displayBooks() {
-  mainContent.innerHTML = "";
-  console.log(myLibrary)
-  myLibrary.books.forEach((book) => {
-    console.log(book)
-    let check = "";
-
-    if (book.isRead) {
-      check = "checked";
-    }
-
-    mainContent.innerHTML += `<div class="card shadow">
-      <div class="title">Title:</div>
-      <div class="card-content title-data">${book.title}</div>
-      <div class="author">Author:</div>
-      <div class="card-content author-data">${book.author}</div>
-      <div class="num-pages">Number of Pages:</div>
-      <div class="card-content num-pages-data">${book.numPages}</div>
-      <div class="read">Read?</div>
-      <input class="card-content read-data checkbox" type="checkbox" id="read-data" name="read-data" ${check}>
-      <button class="remove-btn">Remove Book</button>
-    </div>`;
-  });
-}
-
-// Adds a new book form and checks if there is already an active one
+let mainContent = document.querySelector(".main-content");
 let bookForm = `<form class="book-input" id="book-input" action="#" method="#">
 <div class="card shadow">
     <input class="card-content title-data input" type="text" placeholder="Title" required>
@@ -116,20 +13,106 @@ let bookForm = `<form class="book-input" id="book-input" action="#" method="#">
 </div>
 </form>`;
 
-// this works fine but maybe change formatting
+class Book {
+  constructor(title, author = "Unknown", numPages = "Unknown", read = false) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.read = read;
+  }
+
+  displayBook() {
+    mainContent.innerHTML = "";
+
+    let check = "";
+    if (this.read) {
+      check = "checked";
+    }
+
+    mainContent.innerHTML += `<div class="card shadow">
+        <div class="title">Title:</div>
+        <div class="card-content title-data">${this.title}</div>
+        <div class="author">Author:</div>
+        <div class="card-content author-data">${this.author}</div>
+        <div class="num-pages">Number of Pages:</div>
+        <div class="card-content num-pages-data">${this.numPages}</div>
+        <div class="read">Read?</div>
+        <input class="card-content read-data checkbox" type="checkbox" id="read-data" name="read-data" ${check}>
+        <button class="remove-btn" id="${this.title}">Remove Book</button>
+      </div>`;
+  }
+}
+
+// list of books
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addToLibrary(book) {
+    this.books.push(book);
+  }
+
+  removeBook(button, bookToRemoveId) {
+    // removes html
+    button.parentElement.remove();
+
+    // uses id to find book in library and remove
+    this.books = this.books.filter((e) => e.title !== bookToRemoveId);
+  }
+
+  // loops through mylibrary and displays each book on the page.
+  displayBooks(form) {
+    // Remove form (to be replaced by actual card)
+    form.parentNode.removeChild(form);
+
+    // displays all books
+    myLibrary.books.forEach((book) => {
+      book.displayBook();
+    });
+  }
+}
+
+myLibrary = new Library();
+
+// creates book object adds to library and displays
+function addBook(form) {
+  // Get form values
+  let title = form.querySelector(".title-data").value;
+  let author = form.querySelector(".author-data").value;
+  let numPages = form.querySelector(".num-pages-data").value;
+  let isRead = form.querySelector(".read-data").checked;
+
+  // Create book object
+  let book = new Book(title, author, numPages, isRead);
+
+  // Add book to array
+  myLibrary.addToLibrary(book);
+
+  // Display Books -
+  myLibrary.displayBooks(form);
+}
+
+// add form to add a book to library and creates event listener to remove
 newBooks.addEventListener("click", () => {
   if (document.querySelector(".book-input")) {
     alert("Finish adding the current book before adding another!");
   } else {
     mainContent.innerHTML += bookForm;
-    addBook();
-  }
-});
 
-// Remove a book from the library
-removeButtons.forEach((button) => {
-  button.addEventListener("click", function removeBook() {
-    button.parentElement.remove();
-    // TODO: remove book from library object / list
-  });
+    let form = document.forms["book-input"];
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      addBook(form);
+
+      let removeButtons = document.querySelectorAll(".remove-btn");
+
+      removeButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+          myLibrary.removeBook(button, button.id);
+        });
+      });
+    });
+  }
 });
