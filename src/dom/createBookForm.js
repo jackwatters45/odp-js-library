e47f66bf95c5b8240d6af8cc94926d700756269b
Jack Watters <johnwatt@usc.edu>
2022-12-27 22:10:34 -0800

    create book form function

--- src/dom/createBookForm.js
+++ src/dom/createBookForm.js
@@ -1,13 +1,48 @@
 export default () => {
-  const bookForm = `<form class="book-input" id="book-input" action="#" method="#">
-<div class="card shadow">
-    <input class="card-content title-data input" type="text" placeholder="Title" required>
-    <input class="card-content author-data input" type="text" placeholder="Author">
-    <input class="card-content num-pages-data input" type="number" placeholder="Number of Pages">
-    <div class="read">Have you read it?</div>
-    <input class="card-content read-data checkbox"  type="checkbox" id="read-data" name="read-data">
-    <!-- needs button to remove the book from the library -->
-    <input class="add-btn" type="submit" value="Add Book">
-</div>
-</form>`;
-}
+  const mainContent = document.querySelector('.main-content');
+
+  const form = document.createElement('form');
+  form.classList.add('book-input');
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.classList.add('shadow');
+  form.appendChild(card);
+
+  const title = document.createElement('input');
+  title.classList.add('title-input');
+  title.placeholder = 'Title';
+  title.required = 'true';
+  title.type = 'text';
+  card.appendChild(title);
+
+  const author = document.createElement('input');
+  author.classList.add('author-input');
+  author.placeholder = 'Author';
+  author.type = 'text';
+  card.appendChild(author);
+
+  const pages = document.createElement('input');
+  pages.classList.add('pages-input');
+  pages.placeholder = 'Num Pages';
+  pages.type = 'number';
+  card.appendChild(pages);
+
+  const readText = document.createElement('div');
+  readText.innerHTML = 'Have you read it?';
+  readText.classList.add('read-text');
+  const read = document.createElement('input');
+  read.classList.add('read-input');
+  read.type = 'checkbox';
+  read.name = 'read-input';
+  card.appendChild(readText);
+  card.appendChild(read);
+
+  const submit = document.createElement('input');
+  submit.classList.add('add-btn');
+  submit.type = 'submit';
+  submit.value = 'Add Book';
+  card.appendChild(submit);
+
+  mainContent.appendChild(form);
+};
